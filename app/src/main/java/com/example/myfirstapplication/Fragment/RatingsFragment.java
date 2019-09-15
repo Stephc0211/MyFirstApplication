a59ebc9fef67fc2e041772acb77cfb145614283a
Stephc0211 <1393448044@qq.com>
2019-09-14 20:54:54 -0700

    newest upload

--- app/src/main/java/com/example/myfirstapplication/Fragment/RatingsFragment.java
+++ app/src/main/java/com/example/myfirstapplication/Fragment/RatingsFragment.java
@@ -1,11 +1,13 @@
 package com.example.myfirstapplication.Fragment;
 
+import android.content.Intent;
 import android.media.Rating;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,15 +23,21 @@ import java.util.List;
 
 public class RatingsFragment extends Fragment {
 
+
     private ListView mLvRatings;
     private RatingsListAdapter ratingsListAdapter;
-    private List<RatingsItem> mItemList = new ArrayList<>();
+    private List<RatingsItem> mMyItemList = new ArrayList<>(); //mine
+    private List<RatingsItem> mHotItemList = new ArrayList<>(); //hot
+    private List<RatingsItem> mItemList = new ArrayList<>(); //general
 
+    private TextView mTvHot;
+    private TextView mTvMine;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+
 
         View view = inflater.inflate(R.layout.activity_ratings_list, container, false);
         return view;
@@ -41,6 +49,26 @@ public class RatingsFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mLvRatings = getActivity().findViewById(R.id.lv_ratings_list);
 
+        mTvMine = getActivity().findViewById(R.id.tv_ratings_mine);
+        mTvMine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mItemList.clear();
+                mItemList.addAll(mMyItemList);
+                ratingsListAdapter.notifyDataSetChanged();
+            }
+        });
+
+        mTvHot = getActivity().findViewById(R.id.tv_ratings_hot);
+        mTvHot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mItemList.clear();
+                mItemList.addAll(mHotItemList);
+                ratingsListAdapter.notifyDataSetChanged();
+            }
+        });
+
         for (int i = 0; i <20; i++) {
             RatingsItem ratingsItem = new RatingsItem();
             ratingsItem.setProfilePicUrl(R.drawable.pic_profile_pic);
@@ -51,9 +79,25 @@ public class RatingsFragment extends Fragment {
             ratingsItem.setPic3Url(R.mipmap.ic_launcher);
             ratingsItem.setPic4Url(R.mipmap.ic_launcher);
             ratingsItem.setPic5Url(R.mipmap.ic_launcher);
-            mItemList.add(ratingsItem);
-
+            mHotItemList.add(ratingsItem);
         }
+
+        for (int i = 0; i <20; i++) {
+            RatingsItem ratingsItem = new RatingsItem();
+            ratingsItem.setProfilePicUrl(R.drawable.pic_profile_pic);
+            ratingsItem.setUserName("Mine_" + i);
+            ratingsItem.setDescription("description_" + i);
+            ratingsItem.setPic1Url(R.mipmap.ic_launcher);
+            ratingsItem.setPic2Url(R.mipmap.ic_launcher);
+            ratingsItem.setPic3Url(R.mipmap.ic_launcher);
+            ratingsItem.setPic4Url(R.mipmap.ic_launcher);
+            ratingsItem.setPic5Url(R.mipmap.ic_launcher);
+            mMyItemList.add(ratingsItem);
+        }
+
+        mItemList.clear();
+        mItemList.addAll(mHotItemList);
+
 
         ratingsListAdapter = new RatingsListAdapter(getActivity(), R.layout.item_ratings, mItemList);
         mLvRatings.setAdapter(ratingsListAdapter);
